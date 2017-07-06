681dac5edb895db404f1d1b591ff584243e0890d
root <root@localhost.localdomain>
2017-07-07 00:01:46 +0800

    完成上传自定义迷宫 	修改：      lualib/game/custom_maze_manager.lua 	修改：      proto/proto.c2s.sproto

--- lualib/game/custom_maze_manager.lua
+++ lualib/game/custom_maze_manager.lua
@@ -3,6 +3,8 @@ local custom_maze_manager = {}
 local custom_maze_map = {}
 local mongo_manager = require("mongo_manager")
 local maze_info_map = {}
+local client = require "client"
+local cli = client.handler()
 
 function custom_maze_manager.get_player_custom_maze(player_id) 
 	if not custom_maze_map[player_id] then 
@@ -70,6 +72,26 @@ function custom_maze_manager.create_custom_maze(player_id, maze_info)
 	local player_custom_maze = custom_maze_manager.get_player_custom_maze(player_id)
 	custom_maze_manager.save_player_custom_maze(player_id, player_custom_maze) --保存到玩家自定义迷宫列表
 	custom_maze_manager.save_maze_info(maze_id, maze_info)	--保存自定义迷宫
+end
+
+function cli:upload_maze(args)
+	local maze_info = {}
+
+	--todo 检查参数合法性 字符串要检查敏感字
+
+	maze_info.maze_name = args.maze_name
+	maze_info.maze_height = args.maze_height
+	maze_info.maze_width = args.maze_width
+	maze_info.maze_map = args.maze_map
+	maze_info.start_pos_x = args.start_pos_x
+	maze_info.start_pos_y = args.start_pos_y
+	maze_info.end_pos_x = args.end_pos_x
+	maze_info.end_pos_y = args.end_pos_y
+	maze_info.head_line = args.head_line
+	maze_info.head_line_remark = args.head_line_remark
+	maze_info.maze_setting_flag = args.maze_setting_flag
+
+	custom_maze_manager.create_custom_maze(TEST_PLAYER_ID, maze_info)
 end
 
 return custom_maze_manager
